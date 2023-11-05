ea3495ce15c98784e5cd0adaa05b11b5ee01f13f
Liv-1804 <livinnihilist@gmail.com>
2023-11-04 19:46:40 -0700

    add comments and some functions
    +Adding a new list works
    +saving to local storage works
    +task counter works
    +displaying tasks works
    +deleting completed tasks works
    +deleting lists works (needs improvement)
    -maybe add an edit feature

--- script.js
+++ script.js
@@ -10,38 +10,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const newTaskInput = document.querySelector('[new-task-input]');
   const clearCompleteTasksButton = document.querySelector('[clear-complete-tasks-button]');
 
+  //stores and retrieves data from the browser's local storage, so the data is perserved
   const LOCAL_STORAGE_KEY = 'task.lists';
   let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
   let selectedList = null;
 
+  //save() - save the lists 
+  //display() - controls what is displayed in the UI
   function saveAndDisplay() {
     save();
     display();
   }
-  
+
   function save() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(lists));
   }
   
   function display() {
-    renderLists();
+    displayLists();
   
     if (selectedList) {
       listDisplay.style.display = '';
       llistTitle.innerText = selectedList.name;
-      renderTaskCount();
+      displayTaskCount();
       clearElement(tasksDisplay);
-      renderTasks();
+      displayTasks();
     } else {
       listDisplay.style.display = 'none';
     }
   }
   
-  function renderTasks() {
+  //populates the tasks for the selected list
+  function displayTasks() {
+    clearElement(tasksDisplay);
     selectedList.tasks.forEach(task => {
       const taskElement = document.createElement('div');
       taskElement.classList.add('task');
-      const checkbox = taskElement.createElement('input');
+
+      const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.id = task.id;
       checkbox.checked = task.complete;
@@ -49,28 +55,38 @@ document.addEventListener("DOMContentLoaded", function () {
         task.complete = checkbox.checked;
         saveAndDisplay();
       });
-      const label = taskElement.createElement('label');
+      const label = document.createElement('label');
       label.htmlFor = task.id;
       label.textContent = task.name;
+
       taskElement.appendChild(checkbox);
       taskElement.appendChild(label);
       tasksDisplay.appendChild(taskElement);
     });
   }
   
-  function renderTaskCount() {
+  //calculates and displays the number of remaining tasks for the selected list
+  function displayTaskCount() {
     const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length;
     const taskString = incompleteTaskCount === 1 ? "task" : "tasks";
     taskCounter.innerText = `${incompleteTaskCount} ${taskString} remaining`;
   }
   
-  function renderLists() {
+  //displays the lists available to the user
+  function displayLists() {
     clearElement(listsType);
     lists.forEach(list => {
       const listElement = document.createElement('li');
       listElement.dataset.listId = list.id;
       listElement.classList.add('list-name');
       listElement.textContent = list.name;
+
+      // Add the "Delete List" button
+      const deleteButton = document.createElement('button');
+      deleteButton.classList.add('btn', 'delete-list');
+      deleteButton.textContent = 'Delete';
+      listElement.appendChild(deleteButton);
+
       if (list === selectedList) {
         listElement.classList.add('active-list');
       }
@@ -82,6 +98,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   
+  //clear the child elements of a given element
   function clearElement(element) {
     while (element.firstChild) {
       element.removeChild(element.firstChild);
@@ -122,5 +139,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // Listen for click events on the entire document
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-list')) {
+      const listId = e.target.parentElement.dataset.listId;
+      deleteList(listId);
+    }
+  });
+  
+  // Function to delete a list
+  function deleteList(listId) {
+    // Find the index of the list with the given ID in the 'lists' array
+    const listIndex = lists.findIndex(list => list.id === listId);
+    if (listIndex !== -1) {
+      // Remove the list from the 'lists' array
+      lists.splice(listIndex, 1);
+      // Save the changes to local storage and update the display
+      saveAndDisplay();
+    }
+  }
+
+  //initializing the application's initial state
   display();
 });
